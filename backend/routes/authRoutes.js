3e1a825a78b60d8804ac221073312b9371e85a3e
jainoos <ammjainoos@gmail.com>
2025-03-09 16:59:39 +0530

    create validation middleware for student

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,11 +5,12 @@ import {
   registerTeacher,
   loginTeacher,
 } from "../controllers/authController.js";
+import { validateRegistration } from "../middlewares/validation.js";
 
 const router = express.Router();
 
 // Student auth routes
-router.post("/student/register", registerStudent);
+router.post("/student/register", validateRegistration, registerStudent);
 router.post("/student/login", loginStudent);
 
 // Teacher auth routes
