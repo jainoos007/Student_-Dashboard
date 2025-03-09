189b0b10bfee79e58c4f6ee729806a46bc6f21a2
jainoos <ammjainoos@gmail.com>
2025-03-09 07:51:30 +0530

    create schema for teacher

---
+++ backend/models/teacher.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const teacherSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Middleware to hash password before saving
+teacherSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = bcrypt.genSalt(10);
+  this.password = bcrypt.hash(this.password, salt);
+});
+
+// Method to compare entered password with hashed password
+teacherSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default mongoose.model("Teacher", teacherSchema);
